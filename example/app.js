09b2e4f1bfd62c1867e3aa24b3dddd4e85485500
Sarika Phule <sarika.raskar@gmail.com>
2021-10-01 09:54:14 +1000

    adding tests

--- example/app.js
+++ example/app.js
@@ -1,4 +1,24 @@
 const displayHeader = () => {
   return "Basic Arithmetic";
 };
-module.exports = { displayHeader };
+
+
+const addnumbers = (a, b) => {
+    return a + b;
+};
+
+const multiplynumbers = (a, b) => {
+  return a * b;
+};
+
+const dividenumbers = (a, b) => {
+  return a / b;
+};
+
+
+const firstarray = (arr)=>{
+  //let arr = [8, 4, 9 , 0];
+  return arr[0];
+};
+
+module.exports = { displayHeader, addnumbers, multiplynumbers, dividenumbers, firstarray};
--- example/test/apptest.js
+++ example/test/apptest.js
@@ -5,9 +5,34 @@ const assert = require("assert");
 const app = require("../app.js");
 
 describe("Testing All functions", function () {
-  it("Should return Basic Arithmetic", function () {
+  it("Should return the string Basic Arithmetic", function () {
     //let result = dispalyHeader();
     let result = app.displayHeader();
     assert.strictEqual(result, "Basic Arithmetic");
+  });
+
+  it("Should return addition of two numbers", function () {
+    //let result = dispalyHeader();
+    let result = app.addnumbers(2, 4);
+    assert.strictEqual(result, 6);
+  });
+
+  it("Should return multiplication of two numbers", function () {
+    //let result = dispalyHeader();
+    let result = app.multiplynumbers(2, 4);
+    assert.strictEqual(result, 8);
+  });
+
+  it("Should return Division of two numbers", function () {
+    //let result = dispalyHeader();
+    let result = app.dividenumbers(8, 4);
+    assert.strictEqual(result, 2);
+  });
+
+  it("Should return first element in array", function () {
+    //let result = dispalyHeader();
+    let arr = [8, 4, 9 , 0];
+    let result = app.firstarray(arr);
+    assert.equal(result, 8, 'firstarray([8, 4, 9 , 0]) is 8');
   });
 });
